daa262a60a3ad0cb928716fac321eb95d9ef83d3
ismailAchibat <ismailachibat4@gmail.com>
2024-04-24 17:21:38 +0100

    refactor: simplify time calculations

--- src/hooks/usePlatforms.ts
+++ src/hooks/usePlatforms.ts
@@ -1,5 +1,6 @@
 import { useQuery } from "@tanstack/react-query";
 import platforms from "../data/platforms";
+import ms from "ms"
 import APIClient, { FetchResponse } from "../services/api-client";
 
 const apiClient = new APIClient<Platform>('/platforms/lists/parents')
@@ -15,7 +16,7 @@ const usePlatforms = () =>
     useQuery<FetchResponse<Platform>, Error>({
         queryKey: ['platforms'],
         queryFn: apiClient.getAll,
-        staleTime: 24 * 60 * 60 * 1000,
+        staleTime: ms('24 h'),
         initialData: platforms
     })
 
