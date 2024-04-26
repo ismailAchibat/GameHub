58e288fdb392dcfad18f9760a1e306fed627860a
ismailAchibat <ismailachibat4@gmail.com>
2024-04-26 23:21:04 +0100

    refactor- move entity interfaces

--- src/components/PlatformIconList.tsx
+++ src/components/PlatformIconList.tsx
@@ -6,11 +6,11 @@ import {
   FaLinux,
   FaAndroid,
 } from "react-icons/fa";
-import {MdPhoneIphone} from 'react-icons/md';
-import {SiNintendo} from 'react-icons/si'
-import {BsGlobe} from 'react-icons/bs'
+import { MdPhoneIphone } from "react-icons/md";
+import { SiNintendo } from "react-icons/si";
+import { BsGlobe } from "react-icons/bs";
 import { HStack, Icon } from "@chakra-ui/react";
-import { Platform } from "../hooks/usePlatforms";
+import { Platform } from "../entities/Platform";
 import { IconType } from "react-icons/lib";
 
 interface Props {
@@ -18,23 +18,23 @@ interface Props {
 }
 
 const PlatformIconList = ({ platforms }: Props) => {
-    const iconMap:{[key: string]: IconType} ={
-        pc: FaWindows,
-        playstation: FaPlaystation,
-        xbox: FaXbox,
-        nintendo: SiNintendo,
-        mac: FaApple,
-        linux: FaLinux,
-        android: FaAndroid,
-        ios: MdPhoneIphone,
-        web: BsGlobe
-    }
+  const iconMap: { [key: string]: IconType } = {
+    pc: FaWindows,
+    playstation: FaPlaystation,
+    xbox: FaXbox,
+    nintendo: SiNintendo,
+    mac: FaApple,
+    linux: FaLinux,
+    android: FaAndroid,
+    ios: MdPhoneIphone,
+    web: BsGlobe,
+  };
 
   return (
-    <HStack marginY={'10px'}>
-        {platforms.map((platform) => (
-            <Icon key={platform.id} as={iconMap[platform.slug]} color='gray.500'/>
-          ))}
+    <HStack marginY={"10px"}>
+      {platforms.map((platform) => (
+        <Icon key={platform.id} as={iconMap[platform.slug]} color="gray.500" />
+      ))}
     </HStack>
   );
 };
--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -1,22 +1,11 @@
 import { useInfiniteQuery} from "@tanstack/react-query";
 import APIClient, { FetchResponse } from "../services/api-client";
 import ms from "ms"
-import { Platform } from "./usePlatforms";
 import useGameQueryStore from "../store";
+import { Game } from "../entities/Game";
 
 const apiClient = new APIClient<Game>('/games');
 
-
-export interface Game {
-    id: number;
-    name: string;
-    slug: string;
-    description_raw: string;
-    background_image: string;
-    parent_platforms: {platform: Platform}[]
-    metacritic: number
-}
-  
 
 // const useGames = (gameQuery: GameQuery) => 
 //   useQuery<FetchResponse<Game>, Error>({
